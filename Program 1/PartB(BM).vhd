7395cbeeda8948caa1517dc8c7916db03e397e95
Gursharan Kaur <45887638+GursharanKaur08@users.noreply.github.com>
2021-03-07 21:48:59 +0530

    Update PartB(BM).vhd

--- Program 1/PartB(BM).vhd
+++ Program 1/PartB(BM).vhd
@@ -3,14 +3,14 @@ use ieee.std_logic_1164.all;
 
 Entity ha1 is
 Port (a,b : in bit;
-Sum, Carry : out bit);
+      Sum, Carry : out bit);
 end ha1;
 
 Architecture RTL of ha1 is
 begin
-Process(a,b)
+    Process(a,b)
 begin
-Sum <= a xor b;
-Carry <= a and b;
+    Sum <= a xor b;
+    Carry <= a and b;
 end Process;
 end RTL;
