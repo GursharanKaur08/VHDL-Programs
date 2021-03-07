8e792284e60fedafb47b65afa6c08bf9a019db93
Gursharan Kaur <45887638+GursharanKaur08@users.noreply.github.com>
2021-03-07 21:46:02 +0530

    Update PartB(DFM).vhd

--- Program 1/PartB(DFM).vhd
+++ Program 1/PartB(DFM).vhd
@@ -3,11 +3,11 @@ use ieee.std_logic_1164.all;
 
 Entity ha1 is
 Port (a,b : in bit;
-Sum, Carry : out bit);
+      Sum, Carry : out bit);
 end ha1;
 
 Architecture RTL of ha1 is
 begin
-Sum <= a xor b;
-Carry <= a and b;
+    Sum <= a xor b;
+    Carry <= a and b;
 end RTL;
