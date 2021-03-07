da8ff71c8ab09fcfd9338ff83701fada0c2279f3
Gursharan Kaur <45887638+GursharanKaur08@users.noreply.github.com>
2021-03-07 22:02:47 +0530

    Update PartB.vhd

--- Program 2/PartB.vhd
+++ Program 2/PartB.vhd
@@ -3,24 +3,23 @@ use ieee.std_logic_1164.all;
 
 Entity decoder1 is
 Port [A, B, E : in bit;
-    D : out bit_vector(0 to 3)];
+      D : out bit_vector(0 to 3)];
 end decoder1;
 
 Architecture RTL of decoder1 is
 begin
     Process (A, B, E)
-    Variable ABAR, BBAR: bit;
-    
+Variable ABAR, BBAR: bit;
 begin
     ABAR=not A;
     BBAR=not B;
     if E=1
-    D(0)<=ABAR and BBAR;
-    D(1)<=ABAR and B;
-    D(2)<=A and BBAR;
-    D(3)<=A and B;
+        D(0)<=ABAR and BBAR;
+        D(1)<=ABAR and B;
+        D(2)<=A and BBAR;
+        D(3)<=A and B;
     else
-    D<=”0000”;
+        D<=”0000”;
     end if;
     end Process;
 end RTL;
