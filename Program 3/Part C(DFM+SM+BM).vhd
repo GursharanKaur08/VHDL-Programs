84bf138de6963839db2696ed07569b0848d88ba5
Gursharan Kaur <45887638+GursharanKaur08@users.noreply.github.com>
2021-03-09 23:46:03 +0530

    Update Part C(DFM+SM+BM).vhd

--- Program 3/Part C(DFM+SM+BM).vhd
+++ Program 3/Part C(DFM+SM+BM).vhd
@@ -28,12 +28,13 @@ end RTL;
 
 library ieee;
 use ieee.std_logic_1164.all;
+
 Entity XOR2 IS
 Port(a,b: in std_logic;
-c: out std_logic);
+     c: out std_logic);
 end XOR2;
+
 Architecture RTL of XOR2 is
 begin
-c<= a XOR b;
+    c<= a XOR b;
 end RTL;
-Conclusion:
