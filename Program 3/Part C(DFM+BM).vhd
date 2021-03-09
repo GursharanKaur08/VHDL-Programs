4f512085b0735e78d26627e050dbc833b9037e38
Gursharan Kaur <45887638+GursharanKaur08@users.noreply.github.com>
2021-03-09 23:41:16 +0530

    Update Part C(DFM+BM).vhd

--- Program 3/Part C(DFM+BM).vhd
+++ Program 3/Part C(DFM+BM).vhd
@@ -20,11 +20,13 @@ end RTL;
 
 library ieee;
 use ieee.std_logic_1164.all;
+
 Entity fulladder is
 Port(a,b,c: in bit;
-sum,carry: out bit);
+     sum,carry: out bit);
 end fulladder;
+
 Architecture data of fulladder is
 begin
-sum<= a xor b xor c;
+    sum<= a xor b xor c;
 end data;
