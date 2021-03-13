14c8db80e0a39f5668d168ec551ea6ee8a702fc7
Gursharan Kaur <45887638+GursharanKaur08@users.noreply.github.com>
2021-03-13 22:03:12 +0530

    Update JK Flipflop.vhd

--- Program 8/JK Flipflop.vhd
+++ Program 8/JK Flipflop.vhd
@@ -1,25 +1,27 @@
 Library ieee;
 Use ieee.std_logic_1164.all;
+
 Entity JK_ff is
 Port (J,K,clk : in std_logic; q, qb : out std_logic );
 End JK_ff;
+
 Architecture rtl of jk_ff is
-Begin
+    Begin
 Process (clk)
-Variable temp : std_logic;
-begin
+    Variable temp : std_logic;
+    begin
 If clk'event and clk='1' then
 If (J='0' and K='0') then
-temp := temp;
+    temp := temp;
 elsif ( J='1' and K='1') then
-temp := not temp;
+    temp := not temp;
 elsif (J='0' and K='1') then
-temp := '0';
+    temp := '0';
 else
-temp := '1';
+    temp := '1';
 end if;
 end if;
-q<= temp;
-qb<= not temp;
+    q<= temp;
+    qb<= not temp;
 end process;
 end rtl;
