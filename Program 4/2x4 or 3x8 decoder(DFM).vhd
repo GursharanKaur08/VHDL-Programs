54912f62f3126df36777d65fc93fbef830880f12
Gursharan Kaur <45887638+GursharanKaur08@users.noreply.github.com>
2021-03-27 23:11:11 +0530

    Create 2x4 or 3x8 decoder(DFM).vhd

---
+++ Program 4/2x4 or 3x8 decoder(DFM).vhd
@@ -0,0 +1,20 @@
+Library ieee;
+use ieee.std_logic_1164.all;
+
+Entity decoder2 is
+Port [A, B, En: in bit;
+      D: out bit_vector(o to 3)];
+end decoder2;
+
+Architecture RTL of decoder1 is
+Signal ABAR, BBAR: bit;
+begin
+    ABAR<=not A;
+    BBAR<=not B;
+    D(0)<=ABAR and BBAR and En;
+    D(1)<=ABAR and B and En;
+    D(2)<=A and BBAR and En;
+    D(3)<=A and B and En;
+End RTL;
+
+
