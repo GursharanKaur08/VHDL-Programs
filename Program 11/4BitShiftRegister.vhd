969202d91ae0294894163f8524d37b91b4706f03
Gursharan Kaur <45887638+GursharanKaur08@users.noreply.github.com>
2021-04-19 23:45:50 +0530

    Update 4BitShiftRegister.vhd

--- Program 11/4BitShiftRegister.vhd
+++ Program 11/4BitShiftRegister.vhd
@@ -19,33 +19,39 @@ end component;
 signal qt: std_logic_vector(3 downto 0);
 begin
     g1: for j in 0 to 3 generate
-    d0: if j=0 generate
-    u0: dff port map(din,rst,clk,qt(j),open);
-    end generate d0;
-d1:if j>0 generate
-u1: dff port map(qt(j-1),rst,clk,qt(j),open);
-end generate d1;
-end generate g1;
+        d0: if j=0 generate
+        u0: dff port map(din,rst,clk,qt(j),open);
+        end generate d0;
+    
+        d1:if j>0 generate
+        u1: dff port map(qt(j-1),rst,clk,qt(j),open);
+        end generate d1;
+    end generate g1;
 q<=qt;
 end rtl;
+
+
 library ieee;
+
 use ieee.std_logic_1164.all;
+
 entity dff is
 port(d,rst,clk: in std_logic;
-q,qbar: out std_logic);
+     q,qbar: out std_logic);
 end dff;
+
 architecture rtl of dff is
 begin
-process(clk)
+    process(clk)
 begin
-if(rising_edge(clk))then
-if(rst='1')then
-q<='0';
-qbar<='1';
-else
-q<=d;
-qbar<= (not d);
-end if;
-end if;
-end process;
+    if(rising_edge(clk))then
+    if(rst='1')then
+        q<='0';
+        qbar<='1';
+    else
+        q<=d;
+        qbar<= (not d);
+    end if; 
+    end if;
+    end process;
 end rtl;
