3d0071c7f4e664db0ade072de3008cd3b0c3ba25
Gursharan Kaur <45887638+GursharanKaur08@users.noreply.github.com>
2021-04-20 23:46:17 +0530

    Update 4BitAsynchronousCounter2.vhd

--- Program 11/4BitAsynchronousCounter2.vhd
+++ Program 11/4BitAsynchronousCounter2.vhd
@@ -1,23 +1,25 @@
 library ieee;
 use ieee.std_logic_1164.all;
+
 entity tff is
 port(rst:in std_logic;
-clk:in std_logic;
-t: in std_logic;
-q: out std_logic);
+     clk:in std_logic;
+     t: in std_logic;
+     q: out std_logic);
 end tff;
+
 architecture rtl of tff is
 begin
-process(rst,clk,t) is
-variable m: std_logic:='0';
-begin
-if(rst='1')then
-m:='0';
-elsif(rising_edge(clk))then
-if(t='1')then
-m:= not m;
-end if;
-end if;
-q<=m;
-end process;
+    process(rst,clk,t) is
+    variable m: std_logic:='0';
+    begin
+        if(rst='1')then
+           m:='0';
+        elsif(rising_edge(clk))then
+        if(t='1')then
+            m:= not m;
+        end if;
+        end if;
+        q<=m;
+    end process;
 end rtl;
