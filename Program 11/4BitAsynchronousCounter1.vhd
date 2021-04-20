0e7f1e57aa2615c14d458d02ac926fc7ce5794f9
Gursharan Kaur <45887638+GursharanKaur08@users.noreply.github.com>
2021-04-20 23:46:12 +0530

    Update 4BitAsynchronousCounter1.vhd

--- Program 11/4BitAsynchronousCounter1.vhd
+++ Program 11/4BitAsynchronousCounter1.vhd
@@ -1,25 +1,31 @@
 library ieee;
 use ieee.std_logic_1164.all;
+
 entity count4bit is
 port(rst,clk: in std_logic;
-q:out std_logic_vector(3 downto 0));
+     q:out std_logic_vector(3 downto 0));
 end count4bit;
+
 architecture rtl of count4bit is
 signal qt:std_logic_vector(3 downto 0);
+
 component tff is
 port(rst:in std_logic;
-clk:in std_logic;
-t: in std_logic;
-q: out std_logic);
+     clk:in std_logic;
+     t: in std_logic;
+     q: out std_logic);
 end component;
+
 begin
-g1: for j in 0 to 3 generate
-d0: if j=0 generate
-dff0:tff port map(rst,clk,'1',qt(j));
+    g1: for j in 0 to 3 generate
+    g1: if j=0 generate
+    dff0:tff port map(rst,clk,'1',qt(j));
 end generate d0;
+
 d1: if j>0 generate
 dffj: tff port map(rst,qt(j-1),'1',qt(j));
 end generate d1;
 end generate g1;
+
 q<=qt;
 end rtl;
