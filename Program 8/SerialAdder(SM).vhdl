6cc8dc7753ffcca71de065844e1df7f002e217be
Gursharan Kaur <45887638+GursharanKaur08@users.noreply.github.com>
2021-04-29 00:01:09 +0530

    Update SerialAdder(SM).vhdl

--- Program 8/SerialAdder(SM).vhdl
+++ Program 8/SerialAdder(SM).vhdl
@@ -30,33 +30,5 @@ begin
     cout<=c2;
 end RTL;
 
-library ieee;
-use ieee.std_logic_1164.all;
 
-entity FA is
-port(A,B,CIN: in std_logic;
-     sum,cout: out std_logic);
-end FA;
 
-architecture RTL of FA is
-begin
-    sum<= A xor B xor CIN;
-    cout<= (A and B) or (B and CIN) or (CIN and A);
-end RTL;
-
-library ieee;
-use ieee.std_logic_1164.all;
-
-entity DFF is
-port(D,clk: in std_logic;
-     q,qbar: out std_logic);
-end DFF;
-
-architecture RTL of DFF is
-begin
-    G1: block(clk'event and clk='1')
-begin
-    q<=guarded D;
-    qbar<=guarded not D;
-end block G1;
-end RTL;
