6beabd5e957ba33132bf0c4a640e2261a6828375
Gursharan Kaur <45887638+GursharanKaur08@users.noreply.github.com>
2021-04-29 00:00:23 +0530

    Update FullAdder(DFM+SM+BM).vhd

--- Program 3/FullAdder(DFM+SM+BM).vhd
+++ Program 3/FullAdder(DFM+SM+BM).vhd
@@ -26,15 +26,4 @@ begin
 end process;
 end RTL;
 
-library ieee;
-use ieee.std_logic_1164.all;
 
-Entity XOR2 IS
-Port(a,b: in std_logic;
-     c: out std_logic);
-end XOR2;
-
-Architecture RTL of XOR2 is
-begin
-    c<= a XOR b;
-end RTL;
