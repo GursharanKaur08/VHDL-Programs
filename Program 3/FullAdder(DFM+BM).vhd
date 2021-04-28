55d12a547cf308391c8f01ee66ab4fc412408c0c
Gursharan Kaur <45887638+GursharanKaur08@users.noreply.github.com>
2021-04-28 23:59:58 +0530

    Update FullAdder(DFM+BM).vhd

--- Program 3/FullAdder(DFM+BM).vhd
+++ Program 3/FullAdder(DFM+BM).vhd
@@ -18,15 +18,3 @@ begin
     COUT <= (A AND B) OR (A AND CIN) OR (A AND CIN);
 end RTL;
 
-library ieee;
-use ieee.std_logic_1164.all;
-
-Entity fulladder is
-Port(a,b,c: in bit;
-     sum,carry: out bit);
-end fulladder;
-
-Architecture data of fulladder is
-begin
-    sum<= a xor b xor c;
-end data;
